7bd7b9fa30babda5e6de8405f8c81c7f0b9df7a3
msl <maxsloeffler@gmail.com>
2024-05-02 22:05:31 -0700

    kinda works

--- apps/nextjs/src/app/[cacheKey]/page.tsx
+++ apps/nextjs/src/app/[cacheKey]/page.tsx
@@ -1,30 +1,44 @@
-import { cache } from "react";
-import { env } from "~/env";
-
+import { auth, signIn } from "@acme/auth";
+import { Button } from "@acme/ui/button";
 import type { Page } from "../types";
 import ParentComponent, { HOME_KEY } from "../ParentComponent";
-
-const loadPage = cache(async (cacheKey: string) => {
-  try {
-    const url = new URL(`/api/load-page?cacheKey=${cacheKey}`, env.NEXT_PUBLIC_API_BASE_URL).toString();
-    const response = await fetch(url);
-    const fetchedPage = await response.json() as Page;
-    return fetchedPage;
-  } catch (error) {
-    console.error("Error fetching page:", error);
-    return null;
-  }
-});
-
+import { env } from "~/env";
 
 const CacheKeyPage = async ({ params }: { params: { cacheKey: string } }) => {
-  const page = await loadPage(params.cacheKey);
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <div>
+        <p>You need to be logged in to view this page.</p>
+        <form>
+          <Button
+            size="lg"
+            formAction={async () => {
+              "use server";
+              await signIn("discord");
+            }}
+          >
+            Sign in with Discord
+          </Button>
+        </form>
+      </div>
+    );
+  }
+
+  let page: Page | null = null;
+  try {
+    const { cacheKey } = params;
+    const response = await fetch(`${env.NEXT_PUBLIC_API_BASE_URL}/api/load-page?cacheKey=${cacheKey}`);
+    page = await response.json();
+  } catch (error) {
+    console.error("Error fetching page:", error);
+  }
 
   if (!page) {
     if (params.cacheKey !== HOME_KEY) {
       console.log("page not found for cache key", params.cacheKey);
     }
-
     return <ParentComponent />;
   }
 
