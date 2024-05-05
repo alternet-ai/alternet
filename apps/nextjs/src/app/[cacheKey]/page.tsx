2b7af77177e438a30a77227877a9b2c219884b6c
msl <maxsloeffler@gmail.com>
2024-05-04 22:27:20 -0700

    experiment with metadata

--- apps/nextjs/src/app/[cacheKey]/page.tsx
+++ apps/nextjs/src/app/[cacheKey]/page.tsx
@@ -1,14 +1,46 @@
 import { auth } from "@acme/auth";
+
 import type { Page } from "../types";
-import ParentComponent, { HOME_KEY } from "../ParentComponent";
 import { env } from "~/env";
 import LoginComponent from "../_components/login";
+import ParentComponent, { HOME_KEY } from "../ParentComponent";
 
 interface SearchParams {
   profile?: string;
 }
 
-const CacheKeyPage = async ({ params, searchParams }: { params: { cacheKey: string }, searchParams?: SearchParams }) => {
+export function generateMetadata({ params }: { params: { cacheKey: string } }) {
+  const cacheKey = params.cacheKey;
+
+  return {
+    metadataBase: new URL(
+      env.VERCEL_ENV === "production"
+        ? "https://alternet.ai"
+        : "http://localhost:3000",
+    ),
+    title: "alternet",
+    description: "dream play create",
+    openGraph: {
+      title: "alternet",
+      description: "dream play create",
+      url: "https://alternet.ai",
+      siteName: "alternet",
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@alternet_ai",
+      creator: "@maxsloef",
+    },
+  };
+}
+
+const CacheKeyPage = async ({
+  params,
+  searchParams,
+}: {
+  params: { cacheKey: string };
+  searchParams?: SearchParams;
+}) => {
   const session = await auth();
 
   if (!session) {
@@ -18,8 +50,10 @@ const CacheKeyPage = async ({ params, searchParams }: { params: { cacheKey: stri
   let page: Page | null = null;
   try {
     const { cacheKey } = params;
-    const response = await fetch(`${env.NEXT_PUBLIC_API_BASE_URL}/api/load-page?cacheKey=${cacheKey}`);
-    page = await response.json() as Page;
+    const response = await fetch(
+      `${env.NEXT_PUBLIC_API_BASE_URL}/api/load-page?cacheKey=${cacheKey}`,
+    );
+    page = (await response.json()) as Page;
   } catch (error) {
     console.error("Error fetching page:", error);
   }
@@ -32,7 +66,8 @@ const CacheKeyPage = async ({ params, searchParams }: { params: { cacheKey: stri
   }
 
   // Check if 'profile' query parameter is present and pass it as a prop
-  const profileProp = searchParams?.profile !== undefined ? { profile: true } : {};
+  const profileProp =
+    searchParams?.profile !== undefined ? { profile: true } : {};
 
   return <ParentComponent initialPage={page} {...profileProp} />;
 };
