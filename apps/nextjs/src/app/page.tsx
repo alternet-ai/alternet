ea30d3a445829fc12703816d8f46f75139c55045
msl <maxsloeffler@gmail.com>
2024-05-04 22:59:14 -0700

    fix /

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,7 +1,34 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
+
+import { env } from "~/env";
+import { HOME_KEY, HOME_PAGE } from "./static/constants";
+
+export function generateMetadata() {
+  const url = new URL(env.NEXT_PUBLIC_API_BASE_URL + "/" + HOME_KEY);
+  const page = HOME_PAGE;
+
+  const metadata = {
+    metadataBase: new URL(env.NEXT_PUBLIC_API_BASE_URL),
+    title: `alternet`,
+    description: `dream play create`,
+    openGraph: {
+      title: `${page.title}`,
+      description: `${page.prompt}`,
+      url,
+      siteName: `${page.title}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@alternet_ai",
+      creator: "@maxsloef",
+    },
+  };
+
+  return metadata;
+}
 
 const HomePage = () => {
-  redirect('/home');
+  redirect("/home");
 };
 
 export default HomePage;
