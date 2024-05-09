fd621074e29c89175a4d43d75fbcb1a256c12c6e
msl <maxsloeffler@gmail.com>
2024-05-08 17:56:56 -0700

    limit logo to 25% of width

--- apps/nextjs/src/app/_components/logo.tsx
+++ apps/nextjs/src/app/_components/logo.tsx
@@ -7,11 +7,12 @@ const FloatingLogo = () => {
       src="/alternet-cloud.png"
       alt="alternet.ai watermark"
       width={200}
-      height={200}
+      height={100}
       style={{
         position: "absolute",
         bottom: "1rem",
         right: "1.5rem",
+        maxWidth: "25vw",
       }}
     />
   );
