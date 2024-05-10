bf6fbd9d5fa9ba50377fa0639f09f255562a56dd
msl <maxsloeffler@gmail.com>
2024-05-10 16:53:49 -0700

    default metadata generation

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,38 +1,47 @@
 import type { Metadata, Viewport } from "next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
+import { SessionProvider } from "next-auth/react";
 
 import { cn } from "@acme/ui";
 import { ThemeProvider } from "@acme/ui/theme";
 import { Toaster } from "@acme/ui/toast";
-import { SessionProvider } from "next-auth/react"
 
 import { TRPCReactProvider } from "~/trpc/react";
 
 import "~/app/globals.css";
 
 import { env } from "~/env";
+import { HOME_KEY, HOME_PAGE } from "./static/constants";
+import { DEPLOYMENT_URL } from "./utils/url";
 
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    env.VERCEL_ENV === "production"
-      ? "https://alternet.ai"
-      : "http://localhost:3000",
-  ),
-  title: "alternet",
-  description: "dream play create",
-  openGraph: {
-    title: "alternet",
-    description: "dream play create",
-    url: "https://alternet.ai",
-    siteName: "alternet",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@alternet_ai",
-    creator: "@maxsloef",
-  },
-};
+export function generateMetadata() {
+  const url = new URL(DEPLOYMENT_URL + "/" + HOME_KEY);
+
+  const page = HOME_PAGE;
+
+  const imageUrl = `${env.NEXT_PUBLIC_SCREENSHOT_BUCKET_URL}/${HOME_KEY}.png`;
+
+  const metadata = {
+    metadataBase: new URL(DEPLOYMENT_URL),
+    title: `alternet`,
+    description: `dream play create`,
+    openGraph: {
+      title: `${page.title}`,
+      description: `${page.prompt}`,
+      url,
+      siteName: `${page.title}`,
+      images: [{ url: `${imageUrl}`, width: 630, height: 1200 }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@alternet_ai",
+      creator: "@maxsloef",
+      images: [`${imageUrl}`],
+    },
+  };
+  return metadata;
+}
 
 export const viewport: Viewport = {
   themeColor: [
@@ -51,12 +60,12 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           GeistSans.variable,
           GeistMono.variable,
         )}
-        >
+      >
         <SessionProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <TRPCReactProvider>{props.children}</TRPCReactProvider>
-          <Toaster />
-        </ThemeProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <TRPCReactProvider>{props.children}</TRPCReactProvider>
+            <Toaster />
+          </ThemeProvider>
         </SessionProvider>
       </body>
     </html>
--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,38 +1,5 @@
 import { redirect } from "next/navigation";
 
-import { env } from "~/env";
-import { HOME_KEY, HOME_PAGE } from "./static/constants";
-import { DEPLOYMENT_URL } from "./utils/url";
-
-export function generateMetadata() {
-  const url = new URL(DEPLOYMENT_URL + "/" + HOME_KEY);
-
-  const page = HOME_PAGE;
-
-  // Check if an image already exists in the bucket
-  const imageUrl = `${env.NEXT_PUBLIC_SCREENSHOT_BUCKET_URL}/${HOME_KEY}.png`;
-
-  const metadata = {
-    metadataBase: new URL(DEPLOYMENT_URL),
-    title: `alternet`,
-    description: `dream play create`,
-    openGraph: {
-      title: `${page.title}`,
-      description: `${page.prompt}`,
-      url,
-      siteName: `${page.title}`,
-      images: [{ url: `${imageUrl}`, width: 630, height: 1200 }],
-    },
-    twitter: {
-      card: "summary_large_image",
-      site: "@alternet_ai",
-      creator: "@maxsloef",
-      images: [`${imageUrl}`],
-    },
-  };
-  return metadata;
-}
-
 const HomePage = () => {
   redirect("/home");
 };
